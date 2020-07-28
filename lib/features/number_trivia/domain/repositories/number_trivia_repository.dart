a65c533736f82a11b63657136d7b103f8a67bc69
ggguille <g.granado.g@gmail.com>
2020-07-29 00:29:37 +0200

    Refactor core use cases and implement GetRandomNumberTrivia

--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter_number_trivia_app/core/error/failures.dart';
+import 'package:flutter_number_trivia_app/core/error/failure.dart';
 import 'package:flutter_number_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRepository {
