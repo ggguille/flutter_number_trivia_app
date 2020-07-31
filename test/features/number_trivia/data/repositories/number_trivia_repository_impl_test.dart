71185c540071cf5214ce1ffdf2b3f7e46b0ee203
ggguille <g.granado.g@gmail.com>
2020-07-31 15:03:23 +0200

    Implement getRandomNumberTrivia tests

--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -62,6 +62,15 @@ void main() {
     );
     final NumberTrivia tNumberTrivia = tNumberTriviaModel;
 
+    test('should check if the device is online', () {
+      //arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      // act
+      repository.getConcreteNumberTrivia(tNumber);
+      // assert
+      verify(mockNetworkInfo.isConnected);
+    });
+
     runTestsOnline(() {
       test(
         'should return remote data when the call to remote data source is successful',
@@ -126,6 +135,94 @@ void main() {
               .thenThrow(CacheException());
 
           final result = await repository.getConcreteNumberTrivia(tNumber);
+
+          verifyZeroInteractions(mockRemoteDataSource);
+          verify(mockLocalDataSource.getLastNumberTrivia());
+          expect(result, equals(Left(CacheFailure())));
+        },
+      );
+    });
+  });
+
+  group('getRandomNumberTrivia', () {
+    final tNumberTriviaModel =
+    NumberTriviaModel(number: 123, text: 'test trivia');
+    final NumberTrivia tNumberTrivia = tNumberTriviaModel;
+
+    test('should check if the device is online', () {
+      //arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      // act
+      repository.getRandomNumberTrivia();
+      // assert
+      verify(mockNetworkInfo.isConnected);
+    });
+
+
+    runTestsOnline(() {
+      test(
+        'should return remote data when the call to remote data source is successful',
+            () async {
+          when(mockRemoteDataSource.getRandomNumberTrivia())
+              .thenAnswer((_) async => tNumberTriviaModel);
+
+          final result = await repository.getRandomNumberTrivia();
+
+          verify(mockRemoteDataSource.getRandomNumberTrivia());
+          expect(result, equals(Right(tNumberTrivia)));
+        },
+      );
+
+      test(
+        'should cache the data locally when the call to remote data source is successful',
+            () async {
+          when(mockRemoteDataSource.getRandomNumberTrivia())
+              .thenAnswer((_) async => tNumberTriviaModel);
+
+          await repository.getRandomNumberTrivia();
+
+          verify(mockRemoteDataSource.getRandomNumberTrivia());
+          verify(mockLocalDataSource.cacheNumberTrivia(tNumberTriviaModel));
+        },
+      );
+
+      test(
+        'should return server failure when the call to remote data source is unsuccessful',
+            () async {
+          when(mockRemoteDataSource.getRandomNumberTrivia())
+              .thenThrow(ServerException());
+
+          final result = await repository.getRandomNumberTrivia();
+
+          verify(mockRemoteDataSource.getRandomNumberTrivia());
+          verifyZeroInteractions(mockLocalDataSource);
+          expect(result, equals(Left(ServerFailure())));
+        },
+      );
+    });
+
+    runTestsOffline(() {
+      test(
+        'should return last locally cached data when the cached data is present',
+            () async {
+          when(mockLocalDataSource.getLastNumberTrivia())
+              .thenAnswer((_) async => tNumberTriviaModel);
+
+          final result = await repository.getRandomNumberTrivia();
+
+          verifyZeroInteractions(mockRemoteDataSource);
+          verify(mockLocalDataSource.getLastNumberTrivia());
+          expect(result, equals(Right(tNumberTrivia)));
+        },
+      );
+
+      test(
+        'should return CacheFailure when there is no cached data present',
+            () async {
+          when(mockLocalDataSource.getLastNumberTrivia())
+              .thenThrow(CacheException());
+
+          final result = await repository.getRandomNumberTrivia();
 
           verifyZeroInteractions(mockRemoteDataSource);
           verify(mockLocalDataSource.getLastNumberTrivia());
