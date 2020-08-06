bac8d8ebb27442dc782ee4b52513e02fc53d4a79
ggguille <g.granado.g@gmail.com>
2020-08-06 18:59:54 +0200

    Implement InputConverter core service and fix failure immutability

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
-  List properties = const<dynamic>[];
+  final List<Object> properties;
   Failure(this.properties);
 
   @override
