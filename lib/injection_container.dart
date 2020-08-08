fbae9153210ac0987846e70a17c1caaba8c071a0
ggguille <g.granado.g@gmail.com>
2020-08-08 22:29:42 +0200

    Fix use cases DI

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_number_trivia_app/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_number_trivia_app/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:http/http.dart' as http;
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_number_trivia_app/core/network/network_info.dart';
@@ -7,7 +9,6 @@ import 'package:flutter_number_trivia_app/features/number_trivia/data/datasource
 import 'package:flutter_number_trivia_app/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
 import 'package:flutter_number_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_number_trivia_app/features/number_trivia/presentation/bloc/bloc.dart';
-import 'package:flutter_number_trivia_app/features/number_trivia/presentation/bloc/number_trivia_event.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -24,8 +25,8 @@ Future<void> init() async {
           )
   );
   // Use Cases
-  sl.registerLazySingleton(() => GetTriviaForConcreteNumber(sl()));
-  sl.registerLazySingleton(() => GetTriviaForRandomNumber());
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
   // Repository
   sl.registerLazySingleton<NumberTriviaRepository>(
           () => NumberTriviaRepositoryImpl(
