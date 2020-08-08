e7cecce69a1ca06944fba56d1c1f5bd22321ef4f
Guille <g.granado.g@gmail.com>
2020-08-09 01:02:29 +0200

    14.user interface (#13)
    * Building Out the Content
    
    * Fix dependency injection
    
    * Fix use cases DI
    
    * Message disaply Empty and Error states
    
    * Display loading state
    
    * Display Trivia loaded state
    
    * Minor refactor  widgets
    
    * Finish trivia controls widget
    
    * TriviaControls use textedittingcontroller text value

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_number_trivia_app/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_number_trivia_app/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:http/http.dart' as http;
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_number_trivia_app/core/network/network_info.dart';
@@ -6,12 +8,11 @@ import 'package:flutter_number_trivia_app/features/number_trivia/data/datasource
 import 'package:flutter_number_trivia_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_number_trivia_app/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
 import 'package:flutter_number_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:flutter_number_trivia_app/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
-import 'package:flutter_number_trivia_app/features/number_trivia/presentation/bloc/number_trivia_event.dart';
+import 'package:flutter_number_trivia_app/features/number_trivia/presentation/bloc/bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final sl = GetIt.asNewInstance();
+final sl = GetIt.instance;
 
 Future<void> init() async {
   //! Features - Number Trivia
@@ -24,8 +25,8 @@ Future<void> init() async {
           )
   );
   // Use Cases
-  sl.registerLazySingleton(() => GetTriviaForConcreteNumber(sl()));
-  sl.registerLazySingleton(() => GetTriviaForRandomNumber());
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
   // Repository
   sl.registerLazySingleton<NumberTriviaRepository>(
           () => NumberTriviaRepositoryImpl(
